c6a45007d414ef9d0cc0cbcbe0d834872fe4aaa3
Chirayu31 <chirayupancholi17@gmail.com>
2024-04-13 22:16:58 +0530

    Update image rendering in ArticleCard.svelte

--- src/lib/components/ArticleCard.svelte
+++ src/lib/components/ArticleCard.svelte
@@ -9,7 +9,7 @@
 	<div
 		class="w-full flex bg-white gap-5 py-3 px-5 rounded-2xl mx-2 sm:mx-5 md:mx-10 cursor-pointer hover:scale-[1.005] transition-transform duration-200 ease-in-out"
 	>
-		<img src={image} alt="blog" class="w-12 h-12 bg-yellow-300 rounded-full" />
+		<img src={image} alt="blog" class="w-12 h-12 bg-yellow-300 rounded-full object-cover" />
 		<div>
 			<h3 class="font-medium text-base sm:text-lg">
 				{heading.length > 25 ? `${heading.slice(0, 25)}...` : heading}
